ed40a4d37667a3846ca1cb0ff31d43f8c5e08d53
shaelae <69851498+shaelae@users.noreply.github.com>
2020-08-18 21:36:11 +0300

    Create Main.java

---
+++ Task_1.5/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float TC = scan.nextFloat();
+        float TF = ((TC *9) / 5) + 32;
+        System.out.println(TF);
+    }
+}
