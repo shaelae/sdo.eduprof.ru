253fee63c2b2871ad7eee35f4561006aa668846f
shaelae <69851498+shaelae@users.noreply.github.com>
2020-08-20 12:31:39 +0300

    Create Main.java

---
+++ Task_2.1/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static int min(int a, int b, int c) {
+        if (a > b) {
+            if (b > c) return c;
+            else if (a == b) {
+                if (a > c) return c;
+                else return a;
+            } else return b;
+        }
+        else return a;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
