0075a7379fdc97e4f435bd051e0f44e6da051210
shaelae <69851498+shaelae@users.noreply.github.com>
2020-08-24 14:50:25 +0300

    Create Main.java

---
+++ Task_2.8/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String n = "8";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(n);
+            n += '8';
+        }
+    }
+}
