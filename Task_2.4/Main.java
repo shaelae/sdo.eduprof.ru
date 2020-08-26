7605b5ed52176d686cd7cce02a9c530c38e5a2fd
shaelae <69851498+shaelae@users.noreply.github.com>
2020-08-26 15:11:05 +0300

    Update Main.java

--- Task_2.4/Main.java
+++ Task_2.4/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        int sum = nums[0];
-        for (int i=0; i<nums.length; i++) {
-            System.out.println(sum);
-            if (i<nums.length-1) sum += nums[i + 1];
+        int a=0;
+        int i=0;
+        while (i<5) {
+            i++;
+            a+=i;
+            System.out.println(a);
         }
     }
 }
