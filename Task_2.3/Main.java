1a95776f21123102e638e6e9f474f244bfa76465
shaelae <69851498+shaelae@users.noreply.github.com>
2020-08-26 14:54:14 +0300

    Update Main.java

--- Task_2.3/Main.java
+++ Task_2.3/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7,8,9,10};
-        int mult = nums[0];
-        for (int i=0; i<nums.length-1; i++) mult*=nums[i+1];
-        System.out.println(mult);
-        }
-
+        int a=1;
+        for (int i=1;i<10;i++) a = a*(i+1);
+        System.out.println(a);
     }
+}
