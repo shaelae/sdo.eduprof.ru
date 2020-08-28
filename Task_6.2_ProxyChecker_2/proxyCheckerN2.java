4013d13f5ddc9eeb51bbafa49a7a92ce84af0a2f
shaelae <69851498+shaelae@users.noreply.github.com>
2020-08-28 21:46:57 +0300

    Create proxyCheckerN2.java

---
+++ Task_6.2_ProxyChecker_2/proxyCheckerN2.java
@@ -0,0 +1,63 @@
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class proxyCheckerN2 {
+    public static void main(String[] args) throws Exception {
+
+        FileReader reader = new FileReader("C:\\Users\\nakor\\Desktop\\ip.txt");
+        int c;
+        String iplist = "";
+
+        while((c=reader.read())!=-1){
+            iplist += (char) c;
+        }
+
+        String[] ipListArray = iplist.split("\r");
+
+        for (int i=0;i< ipListArray.length;i++) {
+            String[] ipListArrayElement = ipListArray[i].split(":");
+            String ip = ipListArrayElement[0].trim();
+            int port = Integer.parseInt(ipListArrayElement[1]);
+            Thread t1 = new Thread(new myRunnableClass(ip,port));
+            t1.start();
+        }
+    }
+}
+
+class myRunnableClass implements Runnable {
+
+    String ip = "";
+    int port;
+
+    public myRunnableClass(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
+        try {
+
+            URLConnection connection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
+
+            InputStream is = connection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+            char[] buffer = new char[256];
+            int rc;
+
+            StringBuilder sb = new StringBuilder();
+
+            while ((rc = reader.read(buffer)) != -1)
+                sb.append(buffer, 0, rc);
+
+            reader.close();
+            System.out.println(Thread.currentThread().getName()+" == "+ip+":"+port+" доступен");
+        } catch (Exception e) { System.out.println(Thread.currentThread().getName()+" == "+ip+":"+port+" недоступен"); }
+        }
+    }
