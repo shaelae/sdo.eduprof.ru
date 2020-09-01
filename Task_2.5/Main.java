c323687e135c9f1cb4f2f9740dc607a09d20413d
shaelae <69851498+shaelae@users.noreply.github.com>
2020-09-02 01:23:52 +0300

    Update Main.java

--- Task_2.5/Main.java
+++ Task_2.5/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        //Пока не решил, см. 2.6
-
+        String[] words = {"Мама", "Мыла", "Раму"};
+        int n = words.length;
+        int combs = n*(n-1);
+        String[] phrase = new String[combs];
+        for (int i = 0; i < combs; i++) {
+            if (i%2>0) {
+                // Можно было написать вывод прямо здесь (и в else),
+                // но решил писать во второй массив. Зато потом оттуда
+                // вне цикла можно готовые слова забрать.
+                phrase[i] = words[(Math.abs(i%n-n)-1)]+words[(Math.abs((i+1)%n-n)-1)]+words[(Math.abs((i+2)%n-n)-1)];
+            }  else {
+                phrase[i] = words[(Math.abs(i%n-n)-1)]+words[(Math.abs((i+2)%n-n)-1)]+words[(Math.abs((i+1)%n-n)-1)];
+            }
+            System.out.println(phrase[i]);
+        }
     }
 }
